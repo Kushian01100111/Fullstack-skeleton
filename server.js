cc6c486dc2c182ef7f69dd08d3c6fbf170f72cdc
Kushian01100111 <comaspedro6@gmail.com>
2022-07-04 14:56:03 -0500

    Server.js

--- server.js
+++ server.js
@@ -2,4 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongodbClient = require('mongodb').MongoClient
+require('dotenv').config()
 
+let db ,
+    dbConectionString = processs.env.DB_STRING,
+    dbName ='Sample_Database',
+    collection ;
+
+mongodbClient.connect(dbConectionString)
+    .then(client =>{
+        console.log("Connected to database")
+        db = client.db(dbName)
+        collection = db.collection('Database_1')
+    })
